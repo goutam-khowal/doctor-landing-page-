34fff230e97f7ffdbf53efa013bdf0d18f02647f
GoutamK1902 <goutamkhowal321@gmail.com>
2025-05-06 18:54:24 +0530

    fix nav theme differences

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -23,12 +23,19 @@ function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const { theme, resolvedTheme } = useTheme();
   const [navImg, setNavImg] = useState(sLetterImg);
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   useEffect(() => {
     resolvedTheme === "dark"
       ? setNavImg(sLetterDarkImg)
       : setNavImg(sLetterImg);
   }, [resolvedTheme]);
+
+  if (!mounted) return null;
 
   return (
     <>
@@ -85,54 +92,6 @@ function Navbar() {
           </Link>
         </div>
       </nav>
-      {/* <Dialog
-        open={mobileMenuOpen}
-        onClose={setMobileMenuOpen}
-        className="lg:hidden"
-      >
-        <div className="fixed inset-0 z-50" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <Image alt="" src={sLetterImg} className="h-8 w-auto" />
-            </a>
-
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon aria-hidden="true" className="size-6" />
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                {navigation.map((item) => (
-                  <Link
-                    key={item.name}
-                    href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                    onClick={() => setMobileMenuOpen(false)}
-                  >
-                    {item.name}
-                  </Link>
-                ))}
-              </div>
-              <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </a>
-              </div>
-            </div>
-          </div>
-        </DialogPanel>
-      </Dialog> */}
       <Dialog
         open={mobileMenuOpen}
         onClose={setMobileMenuOpen}
